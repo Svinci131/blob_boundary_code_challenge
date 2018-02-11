cce9e43ae0c49137a64b281d4da50b080a0db630
Svinci131 <svinci131@gmail.com>
2018-02-11 15:07:57 -0500

    run through pep8

--- blob.py
+++ blob.py
@@ -1,97 +1,102 @@
 from json import dumps
+
+
 def validate(data=None):
-	if not isinstance(data, list):
-		raise Exception('Must provide grid')
-	if len(data) != 10:
-		raise Exception('Grid must contain 10 rows')
-	for row in data:
-		if len(row) != 10:
-			raise Exception('Grid row must contain 10 cells')
+    if not isinstance(data, list):
+        raise Exception('Must provide grid')
+    if len(data) != 10:
+        raise Exception('Grid must contain 10 rows')
+    for row in data:
+        if len(row) != 10:
+            raise Exception('Grid row must contain 10 cells')
+
 
 class Blob():
-	def __init__(self, data):
-		validate(data)
+    def __init__(self, data):
+        validate(data)
 
-		self.grid = data
-		self.boundaries = {
-			'Cell_Reads': 0,
-			'Top': 10,
-			'Left': 10,
-			'Bottom': 0,
-			'Right': 0,
-		}
+        self.grid = data
+        self.boundaries = {
+            'Cell_Reads': 0,
+            'Top': 10,
+            'Left': 10,
+            'Bottom': 0,
+            'Right': 0,
+        }
 
-		self.cells_visited = {}
-		self.find_boundaries()
+        self.cells_visited = {}
+        self.find_boundaries()
 
-	def find_boundaries(self):
-		# Loop through until top edge of the blob is found
-		cell = self._findFirstCell()
+    def find_boundaries(self):
+        # Loop through until top edge of the blob is found
+        cell = self._findFirstCell()
 
-		# Set the top boundary
-		self.boundaries['Top'] = cell['x']
+        # Set the top boundary
+        self.boundaries['Top'] = cell['x']
 
-		"""
-		 From that cell keep going through checking
-		 the adjacent cells, and recording the outer limits
-		 until the outer edges of the blob are reached
-		"""
-		self._check_adjacents(cell['x'], cell['y'])
+        """
+         From that cell keep going through checking
+         the adjacent cells, and recording the outer limits
+         until the outer edges of the blob are reached
+        """
+        self._check_adjacents(cell['x'], cell['y'])
 
-	def _findFirstCell(self):
-		cell = { 'x': None, 'y': None }
-		
-		for i in range(0,10):
-			row = self.grid[i]
-			for j in range(0,10):
-				_cell = row[j]
-				if _cell == 1:
-					cell['x'] = i
-					cell['y'] = j
-					return cell
-				else:
-					self.cells_visited[str(i) + str(j)] = True
+    def _findFirstCell(self):
+        cell = {'x': None, 'y': None}
 
-		raise Exception('Grid does not contain blob')
+        for i in range(0, 10):
+            row = self.grid[i]
+            for j in range(0, 10):
+                _cell = row[j]
+                if _cell == 1:
+                    cell['x'] = i
+                    cell['y'] = j
+                    return cell
+                else:
+                    self._addToCellsVisited(i, j)
 
+        raise Exception('Grid does not contain blob')
 
-	def _check_adjacents(self, x, y):
-		# if right is further right than ever before update
-		if (y > self.boundaries['Right']):
-			self.boundaries['Right'] = y
-		# if left is further left than ever before update
-		if (y < self.boundaries['Left']):
-			self.boundaries['Left'] = y
-		# if down is further down than ever before update
-		if (x > self.boundaries['Bottom']):
-			self.boundaries['Bottom'] = x
+    def _addToCellsVisited(self, x, y):
+        key = '{}{}'.format(x, y)
+        self.cells_visited[key] = True
 
-		# Keep checking the cells to right, left and below the
-		# current cell if you hit hit a zero stop.
-		self._process_cell(x, y + 1, 'right')
-		self._process_cell(x, y - 1, 'left')
-		self._process_cell(x + 1, y, 'down')
+    def _check_adjacents(self, x, y):
+        # if right is further right than ever before update
+        if (y > self.boundaries['Right']):
+            self.boundaries['Right'] = y
+        # if left is further left than ever before update
+        if (y < self.boundaries['Left']):
+            self.boundaries['Left'] = y
+        # if down is further down than ever before update
+        if (x > self.boundaries['Bottom']):
+            self.boundaries['Bottom'] = x
 
-	def _process_cell(self, x, y, direction):
-		# If the x,y coordinates fall outside the grid, return
-		if (y >= len(self.grid) or x >= len(self.grid)):
-			return
-		if (y < 0):
-			return
+        # Keep checking the cells to right, left and below the
+        # current cell if you hit hit a zero stop.
+        self._process_cell(x, y + 1, 'right')
+        self._process_cell(x, y - 1, 'left')
+        self._process_cell(x + 1, y, 'down')
 
-		if (self._was_visited(x, y) == False):
-			# Read cell value and increase read count
-			cell = self.grid[x][y]
-			self.boundaries['Cell_Reads'] += 1
-			self.cells_visited[str(x) + str(y)] = True
+    def _process_cell(self, x, y, direction):
+        # If the x,y coordinates fall outside the grid, return
+        if (y >= len(self.grid) or x >= len(self.grid)):
+            return
+        if (y < 0):
+            return
 
-			if (cell != 0):
-				self._check_adjacents(x, y)
+        if (self._was_visited(x, y) is False):
+            # Read cell value and increase read count
+            cell = self.grid[x][y]
+            self.boundaries['Cell_Reads'] += 1
+            self._addToCellsVisited(x, y)
 
-	def _was_visited(self, x, y):
-		key = str(x) + str(y)
-		return key in self.cells_visited
+            if (cell != 0):
+                self._check_adjacents(x, y)
 
-	def __str__(self):
-		return dumps(self.boundaries)
+    def _was_visited(self, x, y):
+        key = '{}{}'.format(x, y)
+        return key in self.cells_visited
 
+    def __str__(self):
+        return dumps(self.boundaries)
--- test_main.py
+++ test_main.py
@@ -1,128 +1,128 @@
 import unittest
-from blob import Blob 
+from blob import Blob
+
 
 class TestBorderBoundaryFinder(unittest.TestCase):
-	def test_basic(self):
-		test_input = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,1,1,1,0,0,0,0,0],
-			[0,0,1,1,1,1,1,0,0,0],
-			[0,0,1,0,0,0,1,0,0,0],
-			[0,0,1,1,1,1,1,0,0,0],
-			[0,0,0,0,1,0,1,0,0,0],
-			[0,0,0,0,1,0,1,0,0,0],
-			[0,0,0,0,1,1,1,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-		]
+    def test_basic(self):
+        test_input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
 
-		test_output = Blob(test_input)
-		self.assertEqual(test_output.boundaries['Top'], 1)
-		self.assertEqual(test_output.boundaries['Left'], 2)
-		self.assertEqual(test_output.boundaries['Bottom'], 7)
-		self.assertEqual(test_output.boundaries['Right'], 6)
-		self.assertEqual(test_output.boundaries['Cell_Reads'], 44)
+        test_output = Blob(test_input)
+        self.assertEqual(test_output.boundaries['Top'], 1)
+        self.assertEqual(test_output.boundaries['Left'], 2)
+        self.assertEqual(test_output.boundaries['Bottom'], 7)
+        self.assertEqual(test_output.boundaries['Right'], 6)
+        self.assertEqual(test_output.boundaries['Cell_Reads'], 44)
 
-	def test_left_further_than_top(self):
-		test_input = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,1,0,0,0,0,0],
-			[0,0,1,1,1,1,1,0,0,0],
-			[0,0,1,0,0,0,1,0,0,0],
-			[0,0,1,1,1,1,1,0,0,0],
-			[0,0,0,0,1,0,1,0,0,0],
-			[0,0,0,0,1,0,1,0,0,0],
-			[0,0,0,0,1,1,1,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-		]
+    def test_left_further_than_top(self):
+        test_input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
 
-		test_output = Blob(test_input)
-		self.assertEqual(test_output.boundaries['Top'], 1)
-		self.assertEqual(test_output.boundaries['Left'], 2)
-		self.assertEqual(test_output.boundaries['Bottom'], 7)
-		self.assertEqual(test_output.boundaries['Right'], 6)
-	
-	def test_right_spaced(self):
-		test_input = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,1,0,0,0,0,1,0,0],
-			[0,0,1,1,1,1,1,1,0,0],
-			[0,0,1,0,0,0,1,0,0,0],
-			[0,0,1,1,1,1,1,1,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-		]
+        test_output = Blob(test_input)
+        self.assertEqual(test_output.boundaries['Top'], 1)
+        self.assertEqual(test_output.boundaries['Left'], 2)
+        self.assertEqual(test_output.boundaries['Bottom'], 7)
+        self.assertEqual(test_output.boundaries['Right'], 6)
 
-		test_output = Blob(test_input)
-		self.assertEqual(test_output.boundaries['Top'], 1)
-		self.assertEqual(test_output.boundaries['Left'], 2)
-		self.assertEqual(test_output.boundaries['Bottom'], 4)
-		self.assertEqual(test_output.boundaries['Right'], 7)
+    def test_right_spaced(self):
+        test_input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
 
-	def test_whole_grid_blob(self):
-		test_input = [
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1,1,1,1],
-		]
+        test_output = Blob(test_input)
+        self.assertEqual(test_output.boundaries['Top'], 1)
+        self.assertEqual(test_output.boundaries['Left'], 2)
+        self.assertEqual(test_output.boundaries['Bottom'], 4)
+        self.assertEqual(test_output.boundaries['Right'], 7)
 
-		test_output = Blob(test_input)
-		self.assertEqual(test_output.boundaries['Top'], 0)
-		self.assertEqual(test_output.boundaries['Left'], 0)
-		self.assertEqual(test_output.boundaries['Bottom'], 9)
-		self.assertEqual(test_output.boundaries['Right'], 9)
-		self.assertEqual(test_output.boundaries['Cell_Reads'], 100)
+    def test_whole_grid_blob(self):
+        test_input = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
 
-	def test_small_blob(self):
-		test_input = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,1],
-		]
+        test_output = Blob(test_input)
+        self.assertEqual(test_output.boundaries['Top'], 0)
+        self.assertEqual(test_output.boundaries['Left'], 0)
+        self.assertEqual(test_output.boundaries['Bottom'], 9)
+        self.assertEqual(test_output.boundaries['Right'], 9)
+        self.assertEqual(test_output.boundaries['Cell_Reads'], 100)
 
-		test_output = Blob(test_input)
-		self.assertEqual(test_output.boundaries['Top'], 9)
-		self.assertEqual(test_output.boundaries['Left'], 9)
-		self.assertEqual(test_output.boundaries['Bottom'], 9)
-		self.assertEqual(test_output.boundaries['Right'], 9)
-		#self.assertEqual(test_output.boundaries['Cell_Reads'], 0) 
+    def test_small_blob(self):
+        test_input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        ]
 
-	def test_no_blob(self):
-		test_input = [
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0],
-		]
+        test_output = Blob(test_input)
+        self.assertEqual(test_output.boundaries['Top'], 9)
+        self.assertEqual(test_output.boundaries['Left'], 9)
+        self.assertEqual(test_output.boundaries['Bottom'], 9)
+        self.assertEqual(test_output.boundaries['Right'], 9)
 
-		try:
-			test_output = Blob(test_input)
-		except BaseException as e:
-			self.assertEqual(str(e), 'Grid does not contain blob')
+    def test_no_blob(self):
+        test_input = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+        try:
+            test_output = Blob(test_input)
+        except BaseException as e:
+            self.assertEqual(str(e), 'Grid does not contain blob')
 
 if __name__ == '__main__':
     unittest.main()
